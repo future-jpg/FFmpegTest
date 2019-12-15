f3d7246b3989b1ec3143f90e9da2445d293b4dcd
xuleiyu <yuxulei@bytedance.com>
2019-12-15 12:25:21 +0800

    调整目录结构

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # FFmpegTest
+相关教程详见Wiki或者我的博客https://blog.csdn.net/arceushs
---
+++ 视频解码并播放/FFmpegTest/FFmpegTest/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  FFmpegTest
+//
+//  Created by xulei on 2019/12/6.
+//  Copyright © 2019 xulei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
